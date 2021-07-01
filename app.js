a65f88e18368c8cd613df4003216f72cd864371e
raajv <raaj.vardhan@gmail.com>
2021-07-01 19:22:25 +0530

    changes to app.js

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-
-let userName = prompt('Please Enter Your Name')
+function getUsername(){
+    let userName = prompt('Please Enter Your Name');
+    document.write ( 'Welcome ' +  userName  +  '!' )
+    return userName;
+}
+let userName = getUsername();
 console.log(userName)
 
+function getDota(){
+    let likeDota= prompt('Do You Like DOTA?')
+
+    if(likeDota == 'yes'){
+        document.write('YOU ARE MOST WELCOME!')
+    } else if (likeDota !='no'){
+        document.write('<img src="images/go home.gif">')
+    }
+    else {
+        prompt('YOU MUST ENTER YES OR NO')
+    }
+    return likeDota;    
+}
+let likeDota = getDota();
+console.log(likeDota);
+
+function myFunction() {
+    let x =
+    document.getElementById("color");
+    x.style.color = "green";
+    }
 
 
-document.write ( 'Welcome' +  userName  +  '!' )
+
--- app2.js
+++
@@ -1,9 +0,0 @@
-
-let likeDota= prompt('Do You Like DOTA?')
-
-console.log(likeDota)
-if(likeDota == 'yes'){
-    document.write('YOU ARE MOST WELCOME!')
-} else {
-    document.write('<img src="images/go home.gif">')
-}
