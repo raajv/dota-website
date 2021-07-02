9e08f4a5b7bfda78d635e681bdf4f129ed3c9a4a
raajv <raaj.vardhan@gmail.com>
2021-07-02 17:43:06 +0530

    small changes to java

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
     if(likeDota == 'yes'){
         document.write('YOU ARE MOST WELCOME!')
     } else if (likeDota =='no'){
-        document.write('<img src="images/go home.gif">')
+        document.write('<img src="images/go home.gif" width"600">')
     }
     else {
         prompt('YOU MUST ENTER YES OR NO')
@@ -31,17 +31,20 @@ function myGame(){
         
         let correctAnswer= 5;
         let numberOfAttempts = 5;
-        for( let i=0 ; i < numberOfAttempts ;i += 1){
-        let userAnswer = prompt('give a star rating between 1-5');
+        
+        for(let i=0 ; i<numberOfAttempts ; i+=1){
+        let userAnswer = prompt('give a star rating between 1-5')
+        
         while(userAnswer == 0 || userAnswer == 1 || userAnswer == 2 || userAnswer == 3 || userAnswer == 4){
          userAnswer = prompt('give a star rating between 1-5');
-         document.write('<h3>' + '<img src="images/fatality.gif" width="100">' + userAnswer + ' is ' + ' never ' + ' right ' + '</h3>')
+         document.write('<h3>' + '<img src="images/fatality.gif" width="200">' + userAnswer + ' is ' + ' never ' + ' right ' + '</h3>')
         console.log(i);
         }
         if (userAnswer == correctAnswer){
             alert('you got it right!');
-            document.write ('<h2>' + userAnswer + ' is' + ' always' + ' right' + ' !' + '</h2>')
+            document.write ('<h2>' + userAnswer + ' is' + ' always' + ' right' + ' !' + '</h2>');
             break;
+            
 
         }else {
             alert('there is only one answer , you know which one !');
